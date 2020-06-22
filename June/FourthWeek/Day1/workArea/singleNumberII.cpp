fc033d9e29ba8dd7ad3e6a26eb098c7c486dc29c
Madhu5A3 <madhuv299@gmail.com>
2020-06-22 20:39:16 +0530

    Update: Day22 WorkArea

---
+++ June/FourthWeek/Day1/workArea/singleNumberII.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int singleNumberII(int arr[], int n)
+{
+    int ones = 0, twos = 0;
+    int bit;
+    for (int i = 0; i < n; i++)
+    {
+        twos |= (ones & arr[i]);
+        ones ^= arr[i];
+        bit = ~(ones & twos);
+        ones &= bit;
+        twos &= bit;
+    }
+    return ones;
+}
+
+int main()
+{
+    int nums[] = {2, 2, 3, 2};
+    int n = *(&nums + 1) - nums;
+    cout << singleNumberII(nums, n);
+}
