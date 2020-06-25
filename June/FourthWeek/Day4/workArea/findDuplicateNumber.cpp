86e8c00da7118af55ec1329ae93ed498245ece68
Madhu5A3 <madhuv299@gmail.com>
2020-06-25 22:23:46 +0530

    Update: Day24 WorkArea

---
+++ June/FourthWeek/Day4/workArea/findDuplicateNumber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = 0;
+        int fast = 0;
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+
+        int find = 0;
+
+        while(find != slow){
+            slow = nums[slow];
+            find = nums[find];
+        }
+        return find;
+    }
+};
