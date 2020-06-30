acfa5a5a53a09ff95b9d7ade4cdc747b2e6031aa
mmkvdev <madhuv299@gmail.com>
2020-06-30 20:52:32 +0530

    Update: Day26 workArea

---
+++ June/Week-5/Day2/workArea/wordSearchII.js
@@ -0,0 +1,11 @@
+const wordSearchII = (board, words) => {
+    console.table(board);
+    console.table(words);
+};
+
+wordSearchII([
+    ['o', 'a', 'a', 'n'],
+    ['e', 't', 'a', 'e'],
+    ['i', 'h', 'k', 'r'],
+    ['i', 'f', 'l', 'v']
+], ["oath", "pea", "eat", "rain"])
