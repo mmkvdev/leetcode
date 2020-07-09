a57a15fcabffb48fa40f45c945a4ffd6765ba4b8
mmkvdev <madhuv299@gmail.com>
2020-07-09 10:32:55 +0530

    Update: XOR Operation Array Update

--- Problems/XOROperationArray/Submissions/xorOperationArray.js
+++ Problems/XOROperationArray/Submissions/xorOperationArray.js
@@ -3,8 +3,8 @@
  * @param {number} start
  * @return {number}
  */
-var xorOperation = function(n, start) {
-    let xor=0;
-    for(let i=0;i<n;i++) xor ^= (start+2*i);
+var xorOperation = function (n, start) {
+    let xor = 0;
+    for (let i = 0; i < n; i++) xor ^= (start + 2 * i);
     return xor;
 };
