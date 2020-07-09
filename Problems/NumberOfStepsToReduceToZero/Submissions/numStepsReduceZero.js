c79e4e14115b05b0e789b35e37f06ca2c739da54
mmkvdev <madhuv299@gmail.com>
2020-07-09 10:17:26 +0530

    Update: Number of Steps to Reduce a Number to Zero Update

--- Problems/NumberOfStepsToReduceToZero/Submissions/numStepsReduceZero.js
+++ Problems/NumberOfStepsToReduceToZero/Submissions/numStepsReduceZero.js
@@ -2,11 +2,11 @@
  * @param {number} num
  * @return {number}
  */
-var numberOfSteps  = function(num) {
+var numberOfSteps = function (num) {
     let count = 0;
-    while(num) {
+    while (num) {
         if (num % 2 === 0) {
-            num = parseInt(num/2);
+            num = parseInt(num / 2);
             count++;
         }
         else {
