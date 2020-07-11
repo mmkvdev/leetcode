30094d6775d0f824f1023a5e64f1c2e34f13ec6f
mmkvdev <madhuv299@gmail.com>
2020-07-11 20:52:35 +0530

    Update: SubsetsII Submissions

---
+++ Problems/SubsetsII/Submissions/subSetsII.js
@@ -0,0 +1,18 @@
+var duplicateSubsetChecker = (nums) => {
+    return nums.map(JSON.stringify).reverse().filter(function (e, i, a) {
+        return a.indexOf(e, i + 1) === -1;
+    }).reverse().map(JSON.parse);
+};
+
+var subsetsWithDup = function (nums) {
+    let subset = [[]];
+    nums.sort();
+    for (let i = 0; i < nums.length; i++) {
+        subset.forEach((item) => {
+            subset.push([...item, nums[i]]);
+        });
+    }
+
+    // checking for duplicates
+    return duplicateSubsetChecker(subset);
+};
