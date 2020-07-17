841ccef0706f69898dbfced74518b4edb7b1b9c5
mmkvdev <madhuv299@gmail.com>
2020-07-17 18:54:26 +0530

    Update: Day3 Work Area

---
+++ July/Week3/Day3/workArea/topKFrequentElements.js
@@ -0,0 +1,12 @@
+var topKFrequent = function(nums, k) {
+    const map = nums.reduce((map, num) => {
+        const occurence = map.get(num) || 0;
+        map.set(num, occurence + 1);
+        return map;
+    }, new Map());
+
+    return Array.from(map)
+        .sort((a, b) => b[1] - a[1])
+        .map(item => item[0])
+        .slice(0, k);
+};
