64b23ab94037e9957dfb3a3b30851bee9466d962
mmkvdev <madhuv299@gmail.com>
2020-07-18 18:05:47 +0530

    Update: Day4 Work Area

---
+++ July/Week3/Day4/workArea/courseScheduleII.js
@@ -0,0 +1,24 @@
+const inDegrees = Array(numCourses).fill(0);
+for (const [v] of prerequisites) {
+    inDegrees[v]++;
+}
+
+const q = [];
+for (let i = 0; i < inDegrees.length; i++) {
+    const degree = inDegrees[i];
+    if (degree === 0) q.push(i);
+}
+
+const res = [];
+while (q.length) {
+    const u0 = q.shift();
+    numCourses--;
+    res.push(u0);
+    for (const [v, u] of prerequisites) {
+        if (u === u0) {
+            inDegrees[v]--;
+            if (inDegrees[v] === 0) q.push(v);
+        }
+    }
+}
+return numCourses === 0 ? res : [];
