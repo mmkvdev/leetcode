e8669a01fb0e94ed9ba5fc0dd052b9b8bdffbf96
mmkvdev <madhuv299@gmail.com>
2020-07-20 18:08:29 +0530

    Update: Day6 workArea

---
+++ July/Week3/Day6/workArea/removeLinkedListElements.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    if (head === null) return null;
+    head.next = removeElements(head.next, val);
+    return head.val !== val ? head : head.next;
+};
