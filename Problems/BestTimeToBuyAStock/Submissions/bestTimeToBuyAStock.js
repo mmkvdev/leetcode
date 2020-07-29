aaa6e6688e01839c5f084df6e0fbbc03cd846e3c
mmkvdev <madhuv299@gmail.com>
2020-07-29 21:11:48 +0530

    Update: bestTimeToBuyAStock

---
+++ Problems/BestTimeToBuyAStock/Submissions/bestTimeToBuyAStock.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+    let minBuyPrice = Infinity, maxSellPrice = 0;
+
+    for (let price of prices) {
+        if (price < minBuyPrice) minBuyPrice = price;
+        else if (price - minBuyPrice > maxSellPrice) maxSellPrice = price - minBuyPrice;
+    }
+    return maxSellPrice;
+};
