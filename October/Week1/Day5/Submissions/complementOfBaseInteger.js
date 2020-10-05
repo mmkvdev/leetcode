c26a26dc2b7e62131ae54c6691bf0555266ef705
mmkvdev <madhuv299@gmail.com>
2020-10-05 21:15:20 +0530

    func/day5-submissions: Complement of Base 10 Integer

---
+++ October/Week1/Day5/Submissions/complementOfBaseInteger.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} N
+ * @return {number}
+ */
+
+function binaryGenerator(n) {
+    if (n === 0) return 0;
+    s = '';
+    while (n) {
+        s += n % 2;
+        n = parseInt(n / 2);
+    }
+    return s.split('').reverse().join('');
+}
+
+function complement(n) {
+    return n
+        .split('')
+        .map((v) => (v === '0' ? '1' : '0'))
+        .join('');
+}
+
+function number(n) {
+    let complement_int = 0;
+    for (let i = 0; i < n.length; i++) {
+        complement_int += 2 ** i * n[i];
+    }
+
+    return complement_int;
+}
+
+var bitwiseComplement = function (N) {
+    if (N === 0) return 1;
+    return number(complement(binaryGenerator(N)).split('').reverse().join(''));
+};
