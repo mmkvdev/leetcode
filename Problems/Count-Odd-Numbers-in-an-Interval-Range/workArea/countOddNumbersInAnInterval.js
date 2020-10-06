c675dc9924c989db4f18886a34e86053954c5815
mmkvdev <madhuv299@gmail.com>
2020-10-06 21:04:50 +0530

    func-js/workarea: Count Odd Numbers in an Interval Range

---
+++ Problems/Count-Odd-Numbers-in-an-Interval-Range/workArea/countOddNumbersInAnInterval.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function (low, high) {
+    let count = 0;
+    for (let i = low; i <= high; i++) {
+        if (i % 2 !== 0) {
+            count++;
+        } else {
+            continue;
+        }
+    }
+
+    return count;
+};
