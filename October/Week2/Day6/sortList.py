2f9ad4f8195a33567f8b77b7b199ef60bf2495e6
mmkvdev <madhuv299@gmail.com>
2020-10-13 21:51:39 +0530

    func/day6: Sort List

---
+++ October/Week2/Day6/sortList.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head):
+        if not head or not head.next: return head
+        mid = self.getMid(head)
+        left = self.sortList(head)
+        right = self.sortList(mid)
+        return self.merge(left, right)
+    
+    def getMid(self, head):
+        slow, fast = head, head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        mid = slow.next
+        slow.next = None
+        return mid
+    
+    def merge(self, head1, head2):
+        dummy = tail = ListNode(None)
+        while head1 and head2:
+            if head1.val < head2.val:
+                tail.next, tail, head1 = head1, head1, head1.next
+            else:
+                tail.next, tail, head2 = head2, head2, head2.next
+    
+        tail.next = head1 or head2
+        return dummy.next
+        
