06e244d488e323951ccb78a8b97f6501dee4b86d
mmkvdev <madhuv299@gmail.com>
2020-11-15 21:05:59 +0530

    func-workarea/day1: Range Sum of BST

---
+++ November/Week3/Day1/workArea/rangeSumOfBST.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
+        '''def preorder(root):
+            if not root:
+                return
+            
+            print(root.val)
+            preorder(root.left)
+            preorder(root.right)'''
+            
+        '''k = preorder(root).sort()
+        print(k)'''
+        
+        def sumBST(root: TreeNode):
+            
+            if not root:
+                return
+            
+            if low <= root.val <= high:
+                self.res += root.val
+                
+            if root.val > low:
+                sumBST(root.left)
+            
+            if root.val < high:
+                sumBST(root.right)
+                
+        self.res = 0
+        sumBST(root)
+        
+        return self.res
