0b9d065708d27d9729613c60c1ce5bed692487ee
mmkvdev <madhuv299@gmail.com>
2020-12-02 23:28:03 +0530

    func/day2: Linked List Random Node

---
+++ December/Week1/Day2/LinkedListRandomNode.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+
+    def __init__(self, head: ListNode):
+        """
+        @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node.
+        """
+        self.head = head
+
+    def getRandom(self) -> int:
+        """
+        Returns a random node's value.
+        """
+        n, k = 1, 1
+        head, ans = self.head, self.head
+        while head.next:
+            n += 1
+            head = head.next
+            if random.random() < k/n:
+                ans = ans.next
+                k += 1
+                
+        return ans.val
+
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()
+    
