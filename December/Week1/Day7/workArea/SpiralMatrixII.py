fb4bbd586e68a5ae23b7eb97f3ccb1ad0a067a4d
mmkvdev <madhuv299@gmail.com>
2020-12-07 20:48:41 +0530

    func-workarea/day7: Spiral Matrix II

---
+++ December/Week1/Day7/workArea/SpiralMatrixII.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        res = [[0]*n for _ in range(n)]
+        # print(res)
+        x,dx,y,dy = 0,1,0,0
+        
+        for i in range(n*n):
+            res[y][x] = i+1
+            if not 0 <= x+dx < n or not 0 <= y+dy < n or res[y+dy][x+dx] != 0:
+                dx, dy = -dy, dx
+            x,y = x+dx, y+dy
+            
+        # print(res)
+        return res
+            
